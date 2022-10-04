1bd2c30f802382c78e0dfc885a25f088131ded3c
Doug Meurer <douggmeurer@gmail.com>
2022-10-04 11:59:31 -0300

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const apiURLs = {
+  development: "http://localhost:8000",
+  production: "",
+};
+
+const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
+
+api.interceptors.request.use((config) => {
+  const jsonLoggedUser = localStorage.getItem("loggedInUser");
+  const parsedLoggedUser = JSON.parse(jsonLoggedUser || '""');
+
+  if (parsedLoggedUser.token) {
+    config.headers = { Authorization: `Bearer ${parsedLoggedUser.token}` };
+  }
+
+  return config;
+});
+
+export { api };
---
+++ src/pages/UsersPage.js
@@ -0,0 +1,5 @@
+function UsersPage() {
+  return <div></div>;
+}
+
+export default UsersPage;
